abbab13c9c013b283587b7b177ee29dbd27cbf7e
islam abo mokh <alo.jalal.55@gmail.com>
2023-05-02 15:07:17 +0300

    chart bug

--- src/app/cmps/chart/chart.component.ts
+++ src/app/cmps/chart/chart.component.ts
@@ -3,10 +3,13 @@ import {
   OnInit,
   ViewChild,
   AfterViewInit,
+  ElementRef,
   ChangeDetectorRef,
 } from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
 import { debounceTime } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
+
 import { Chart, ChartConfiguration, registerables } from 'chart.js';
 import { BitcoinService } from '../../services/bitcoin.service';
 
@@ -26,6 +29,7 @@ Chart.register(...registerables);
   selector: 'chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartComponent implements OnInit {
   chartTypeChangeSubject = new Subject<string>();
@@ -36,41 +40,63 @@ export class ChartComponent implements OnInit {
   chart: any;
   chartType: ChartType = 'bar';
   private changeDetector: ChangeDetectorRef;
+  @ViewChild('chartCanvas', { static: false })
+  chartCanvas!: ElementRef<HTMLCanvasElement>;
+  private chartTypeChangeSubscription: Subscription;
 
   constructor(
     private bitcoinService: BitcoinService,
     changeDetector: ChangeDetectorRef
   ) {
     this.changeDetector = changeDetector;
+    this.chartTypeChangeSubscription = this.chartTypeChangeSubject
+      .pipe(debounceTime(300))
+      .subscribe((chartType) => this.changeChartType(chartType));
+  }
+
+  ngOnDestroy() {
+    this.chartTypeChangeSubscription.unsubscribe();
   }
 
   async ngOnInit() {
     try {
-      const [marketPriceData, confirmedTransactionsData] = await Promise.all([
-        this.bitcoinService.getMarketPrice(),
-        this.bitcoinService.getConfirmedTransactions(),
-      ]);
+      const marketPriceData = await this.bitcoinService.getMarketPrice();
+      const confirmedTransactionsData =
+        await this.bitcoinService.getConfirmedTransactions();
+
+      console.log('this.marketPriceData', marketPriceData);
+      console.log(
+        'this.confirmedTransactionsData',
+        this.confirmedTransactionsData
+      );
 
       if (!marketPriceData || !confirmedTransactionsData) {
         console.log('Data not available for generating the chart');
         return;
       }
 
-      this.marketPriceData = marketPriceData;
-      this.confirmedTransactionsData = confirmedTransactionsData;
+      this.marketPriceData = marketPriceData.values;
+      this.confirmedTransactionsData = confirmedTransactionsData.values;
+      console.log('this.marketPriceData', this.marketPriceData);
+      console.log(
+        'this.confirmedTransactionsData',
+        this.confirmedTransactionsData
+      );
 
-      this.marketPriceChartData = this.marketPriceData.values.map(
+      this.marketPriceChartData = this.marketPriceData.map(
         ({ x, y }: { x: number; y: number }) => [x, y]
       );
-      this.confirmedTransactionsChartData =
-        this.confirmedTransactionsData.values.map(
-          ({ x, y }: { x: number; y: number }) => [x, y]
-        );
+      this.confirmedTransactionsChartData = this.confirmedTransactionsData.map(
+        ({ x, y }: { x: number; y: number }) => [x, y]
+      );
 
-      this.generateChart();
+      console.log('marketPriceChartData', this.marketPriceChartData);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+  }
+  ngAfterViewInit() {
+    requestAnimationFrame(() => this.generateChart());
   }
 
   changeChartType(value: string) {
@@ -81,25 +107,30 @@ export class ChartComponent implements OnInit {
       this.changeDetector.detectChanges();
     }
   }
-  private generateChart() {
-    if (!this.marketPriceData || !this.confirmedTransactionsData) {
-      console.error('Data not available for generating the chart');
-      return;
-    }
+  private generateChart = () => {
+    console.log('this.marketPriceData', this.marketPriceData);
+    console.log(
+      'this.confirmedTransactionsData',
+      this.confirmedTransactionsData
+    );
+
     const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    console.log('marketPriceData', this.marketPriceChartData);
+
+    console.log('ctx', ctx);
     const chartConfig = {
       type: this.chartType,
       data: {
-        labels: this.marketPriceChartData.map((data) => data[0]),
+        labels: ['red', 'green'],
         datasets: [
           {
-            label: this.marketPriceData.name,
+            label: 'market prices',
             data: this.marketPriceChartData.map((data) => data[1]),
             borderColor: 'rgba(75, 192, 192, 1)',
             borderWidth: 1,
           },
           {
-            label: this.confirmedTransactionsData.name,
+            label: 'confirmed trancs',
             data: this.confirmedTransactionsChartData.map((data) => data[1]),
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
@@ -135,5 +166,5 @@ export class ChartComponent implements OnInit {
     } else {
       this.chart = new Chart(ctx, chartConfig as ChartConfiguration);
     }
-  }
+  };
 }
